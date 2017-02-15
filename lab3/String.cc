a9c81124e376b0c8f3b449f286d1e0be27021758
Hanne Heingård <bajs@hotmail.com>
2017-02-15 15:23:57 +0100

    Hej

--- lab3/String.cc
+++ lab3/String.cc
@@ -17,13 +17,20 @@ template<typename T>
     T string_cast(string s){
         T temp;
         istringstream iss(s);
+
+        iss >> temp;
+
+        if(!iss) {
+            throw invalid_argument("Not an acceptable input");
+        }
+        /*
         iss.exceptions(ios::failbit);
     try {
         iss >> temp;
 
     } catch(...){
         throw invalid_argument("Not an acceptable input");
-    }
+    }*/
 
 
 
@@ -33,10 +40,12 @@ template<typename T>
 
 int main(){
     try {
-        int i = string_cast<int>("a");
+        int i = string_cast<int>("123");
         double d = string_cast<double>("12.34");
         Date date = string_cast<Date>("2015-01-10");
         cout << i << "\n" << d << "\n" << date << endl;
+
+        i = string_cast<int>("a");
     }
     catch (invalid_argument& e) {
         cout << "Error: " << e.what() << endl;
